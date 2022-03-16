f12ed35641cfd6a3ac9ebee5ca51a3c0b1464a7e
Benoit Ferré <benoit.ferre@oise.fr>
2022-03-16 18:42:06 +0100

    win_dhcp_lease - Change ip if reservation exists

--- plugins/modules/win_dhcp_lease.ps1
+++ plugins/modules/win_dhcp_lease.ps1
@@ -385,7 +385,11 @@ if ($state -eq "present") {
             
             $lease_params.addressState = 'Active'
             $lease_params.Confirm = $false
-
+            
+            if ($ip) {
+                $lease_params.ip = $ip
+            }
+            
             if ($duration) {
                 $lease_params.LeaseExpiryTime = (Get-Date).AddDays($duration)
             }
